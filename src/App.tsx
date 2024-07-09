721c0b656e76df60fa908661409abd37a64f14d5
shakilshagu <muhammadshakil1968@gmail.com>
2024-07-09 14:53:48 +0530

    feat:Tailwindintegration

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-     <Typography text="Take a Time Build Your Self 🔥" tag="h1" className="title" style={{fontSize:"30px", color:"white"}}><span>&nbsp; Hi Hello </span></Typography>
-     <Typography text="hello Man we want like the this kind of jobs🔥" tag="h2"/>
+     <Typography text="Take a Time Build Your Self 🔥" tag="h1" className="title bg-white" style={{fontSize:"30px", color:"white"}}><span>&nbsp; Hi Hello </span></Typography>
+     <Typography text="hello Man we want like the this kind of jobs🔥" tag="h2" className='text-[red]'/>
      <Typography text="hello Man we want like the this kind of jobs  this kind activity thi strings" tag="p"/>
     </>
   )
